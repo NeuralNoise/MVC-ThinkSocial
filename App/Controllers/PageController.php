416497b6e8e73859794267cb63f808517a3a1d1f
abrichak <brichak.new@gmail.com>
2017-01-27 15:18:17 +0200

    Update PageController.php

--- App/Controllers/PageController.php
+++ App/Controllers/PageController.php
@@ -2,17 +2,9 @@
 namespace App\Controllers;
 
 use App\Components\ActiveRecord;
+use App\Models\{User, UserAvatarComment, Group, Friend, Message, City, Album, AlbumPhotoComment, NewsComment};
 use App\DataMapperModels\DialogMapper;
 use App\DataMapperModels\RecipientMapper;
-use App\Models\User;
-use App\Models\UserAvatarComment;
-use App\Models\Group;
-use App\Models\Friend;
-use App\Models\Message;
-use App\Models\City;
-use App\Models\Album;
-use App\Models\AlbumPhotoComment;
-use App\Models\NewsComment;
 use Composer\Command\DiagnoseCommand;
 
 /**
@@ -126,7 +118,6 @@ class PageController
         list($albums, $commentPhotosNum) = $this->getAlbumsInfo();
         $commentNewsNum = $this->getNewsCommentsInfo();
         $friendReqs = $this->getFriendRequestsInfo();
-//        $unreadMessagesNum = Message::count(['receiverId' => $this->userId, 'status' => 'unread']);
 
         $unreadMessagesNum = 0;
         $recipientMapper = new RecipientMapper();
